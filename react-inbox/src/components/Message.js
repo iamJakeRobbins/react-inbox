381066366408fa44617433d5f8c759d25395b6a3
Jake Robbins <jrobbinsd@gmail.com>
2017-12-12 11:41:28 -0700

    viewable read status

--- react-inbox/src/components/Message.js
+++ react-inbox/src/components/Message.js
@@ -1,9 +1,27 @@
 import React, { Component } from 'react';
 class Message extends Component{
+	toggleRead(){
+		return this.props.read === true ? 'read' : 'unread'
+	}
 	render(){
+		{console.log(this.toggleRead)}
 		return (
-			<div>
-				{this.props.subject}
+			<div className={`row message ${this.toggleRead()}`}>
+			  <div className="col-xs-1">
+			    <div className="row">
+			      <div className="col-xs-2">
+			        <input type="checkbox" />
+			      </div>
+			      <div className="col-xs-2">
+			        <i className="star fa fa-star-o"></i>
+			      </div>
+			    </div>
+			  </div>
+			  <div className="col-xs-11">
+			    <a href="#">
+						{this.props.subject}
+			    </a>
+			  </div>
 			</div>
 		)
 	}
